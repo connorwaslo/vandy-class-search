709c0a223437f726bd35de053b7cf1ce5e45b697
connorwaslo <connorwaslo@gmail.com>
2019-06-01 02:49:41 -0400

    Navigation framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import config from './apis/firebase-config';
+import firebase from 'firebase';
 import './App.css';
+import {Container} from "@material-ui/core";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+
+firebase.initializeApp(config);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <div>
+          <ul>
+            <Link to='/'>Home</Link>
+          </ul>
+          <ul>
+            <Link to='/Login'>Login</Link>
+          </ul>
+        </div>
+
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+      </Container>
+    </Router>
   );
 }
 
