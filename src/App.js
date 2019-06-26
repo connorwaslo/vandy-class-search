df9be6cac6b5bec3fa5c28aad96bcb2a595aee94
connorwaslo <connorwaslo@gmail.com>
2019-06-26 10:00:02 -0500

    Basic state persistence

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {createStore} from 'redux';
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import login from './ducks/reducers';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import config from './apis/firebase-config';
@@ -14,7 +18,16 @@ import {Provider} from "react-redux";
 
 firebase.initializeApp(config);
 
-const store = createStore(login);
+// Persist data
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2
+};
+
+const pReducer = persistReducer(persistConfig, login);
+const store = createStore(pReducer);
+const persistor = persistStore(store);
 
 class App extends React.Component {
   componentDidMount() {
@@ -31,16 +44,18 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Router basename='/course-search/'>
-          <Container>
-            <Route exact path='/' component={Signup}/>
-            <Route path='/login' component={Login}/>
-            <Route path='/profile-setup' component={ProfileQuestions}/>
-          </Container>
+        <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
+          <Router basename='/course-search/'>
+            <Container>
+              <Route exact path='/' component={Signup}/>
+              <Route path='/login' component={Login}/>
+              <Route path='/profile-setup' component={ProfileQuestions}/>
+            </Container>
 
-          {/*<Route exact path='/' component={Signup} />*/}
-          <Route path='/dashboard' component={Dashboard}/>
-        </Router>
+            {/*<Route exact path='/' component={Signup} />*/}
+            <Route path='/dashboard' component={Dashboard}/>
+          </Router>
+        </PersistGate>
       </Provider>
     );
   }
