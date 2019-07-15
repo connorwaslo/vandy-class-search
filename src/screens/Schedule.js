62c41a8aba10f46348e94e8cf39f9ef614a3cd77
connorwaslo <connorwaslo@gmail.com>
2019-07-15 15:54:07 -0500

    Mock class in schedule
    Uploaded a mock class in the Schedule state just to see how it looks. It's perfect, needs redux integration & prep next.

--- src/screens/Schedule.js
+++ src/screens/Schedule.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
-import {ReactAgenda} from 'react-agenda';
+import {ReactAgenda, ReactAgendaCtrl, guid, Modal} from 'react-agenda';
 import 'react-agenda/build/styles.css';
 import 'react-datetime/css/react-datetime.css';
 
+let now = new Date();
+
 class Schedule extends Component {
-  state = {};
+  state = {
+    items: [  // Some mock classes
+      {
+        _id: guid(),
+        name: 'SPAN 4455 Development of Drama',
+        startDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0),
+        endDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 11, 5)
+      }
+    ]
+  };
 
   render() {
+    const {items} = this.state;
+
     return (
       <div style={this.props.style}>
         <ReactAgenda
+          items={items}
           numberOfDays={5}/>
       </div>
     )
